31063a5c388fd227f1bd0f82b633f0e3c890de2d
Varsha Jangid <varshajangid1999@gmail.com>
2022-09-20 01:18:07 -1000

    setup a new project

--- lib/Constants/constants.dart
+++
@@ -1,4 +0,0 @@
-
-const String MAIN_UI = "main_ui";
-const String SPLASH_SCREEN = "splash_screen";
-const String SETTINGS_SCREEN = "settings_screen";
--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,71 @@
 import 'package:provider/provider.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
-import 'UI/splash_screen.dart';
-import '/UI/settings.dart';
-import '/UI/main_ui.dart';
+import 'src/theming/theme_manager.dart';
 
-import '/Constants/constants.dart';
-import '/Model/theme_model.dart';
-
-void main() => runApp(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider<ThemeModel>(create: (_) => ThemeModel())
-        ],
-        child: const MyApp(),
-      ),
-    );
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyApp();
+void main() {
+  return runApp(
+    ChangeNotifierProvider<ThemeNotifier>(
+      create: (_) => ThemeNotifier(),
+      child: MyApp(),
+    ),
+  );
 }
 
-class _MyApp extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
-    return ChangeNotifierProvider<ThemeModel>(
-      create: (BuildContext context) => ThemeModel(),
-      child: Consumer<ThemeModel>(builder: (context, model, __) {
-        return MaterialApp(
-          initialRoute: SPLASH_SCREEN,
-          routes: <String, WidgetBuilder>{
-            MAIN_UI: (BuildContext context) => MainUI(model),
-            SPLASH_SCREEN: (BuildContext context) =>
-                const AnimatedSplashScreen(),
-            SETTINGS_SCREEN: (BuildContext context) =>
-                Setting(title: "", model: ThemeModel()),
-          },
-          darkTheme: ThemeData.dark(),
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            dividerColor: model.dividerColor,
-            textTheme: Theme.of(context).textTheme.apply(
-                bodyColor: model.textColor, displayColor: model.textColor),
-            appBarTheme: AppBarTheme(color: model.appbarcolor),
-            primaryColor: model.primaryMainColor,
-            colorScheme:
-                ColorScheme.fromSwatch().copyWith(secondary: model.accentColor),
+    return Consumer<ThemeNotifier>(
+      builder: (context, theme, child) => MaterialApp(
+        theme: theme.getTheme(),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Multiple Theme'),
+            actions: [
+              PopupMenuButton<int>(
+                itemBuilder: (context) => [
+                  PopupMenuItem(
+                    value: 1,
+                    child: const Text('Light Theme'),
+                    onTap: () => theme.setLightMode(),
+                  ),
+                  PopupMenuItem(
+                    value: 2,
+                    child: const Text("Dark Theme"),
+                    onTap: () => theme.setDarkMode(),
+                  ),
+                  PopupMenuItem(
+                    value: 3,
+                    child: const Text("Red Theme"),
+                    onTap: () => theme.setRedMode(),
+                  ),
+                  PopupMenuItem(
+                    value: 45,
+                    child: const Text("Green Theme"),
+                    onTap: () => theme.setGreenMode(),
+                  ),
+                ],
+              ),
+            ],
           ),
-          title: 'Flutter Multi Theme',
-        );
-      }),
+          body: Column(
+            children: [
+              const Text(
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+              const SizedBox(height: 20),
+              const Icon(Icons.ac_unit, size: 50),
+              const SizedBox(height: 20),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  "Test Button",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
